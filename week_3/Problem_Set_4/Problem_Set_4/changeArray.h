77a8d0821ed6b5d429e7ff22831e2970e25729f7
LaTiesha Caston <latieshacaston@gmail.com>
2015-08-22 13:31:04 -0400

    commiting

---
+++ week_3/Problem_Set_4/Problem_Set_4/changeArray.h
@@ -0,0 +1,36 @@
+//
+//  changeArray.h
+//  Problem_Set_4
+//
+//  Created by new on 8/22/15.
+//  Copyright (c) 2015 new. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface changeArray : NSObject
+
+
+
+// Question 1
+
+- (NSArray *) capObjectsInArray: (NSArray *) array1;
+
+// Question 2
+
+- (NSArray *) combineArraysOne: (NSArray *) arrayOne
+                        andTwo: (NSArray *) arrayTwo;
+
+
+
+// Question 3
+
+
+- (NSMutableArray *) printOutDictionaryValues: (NSArray *) carArray;
+
+
+
+
+
+
+@end
