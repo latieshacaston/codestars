47d6b281862bab34fc2986d921e4c33f8a5c4c82
latieshacaston <latieshacaston@gmail.com>
2015-09-14 01:39:56 -0400

    week 6

---
+++ week_6/ToDoList/ToDoList/AddToDoItemViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddToDoItemViewController.h
+//  
+//
+//  Created by LaTiesha Caston on 9/13/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "ToDoItem.h"
+
+@interface AddToDoItemViewController : UIViewController
+
+
+
+@property ToDoItem *toDoItem; 
+
+
+@end
---
+++ week_6/ToDoList/ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by LaTiesha Caston on 9/12/15.
+//  Copyright (c) 2015 LaTiesha Caston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
