2c8f905b9b691917ef55e1c03d206abfe7f9b223
Latiesha Caston <aciddrops23@gmail.com>
2015-10-17 15:40:21 -0400

    sigh

---
+++ final_project/FreshPrince/FreshPrince/VideoPlayer.m
@@ -0,0 +1,21 @@
+//
+//  VideoPlayer.m
+//  FreshPrince
+//
+//  Created by Latiesha Caston on 10/17/15.
+//  Copyright © 2015 LaTiesha Caston. All rights reserved.
+//
+
+#import "VideoPlayer.h"
+
+@implementation VideoPlayer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
