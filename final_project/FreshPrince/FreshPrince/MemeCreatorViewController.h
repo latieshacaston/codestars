4d5805f053155beb6263e870dfa6ff48efa24a8f
latieshacaston <aciddrops23@gmail.com>
2015-10-19 04:20:38 -0400

    meme generartor done. Spotify and episode guide tomorrow

--- final_project/FreshPrince/FreshPrince/MemeCreatorViewController.h
+++ final_project/FreshPrince/FreshPrince/MemeCreatorViewController.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MemeCreatorViewController : UIViewController
+@interface MemeCreatorViewController : UIViewController <UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
 
 
 @property (weak, nonatomic) IBOutlet UIImageView *memeImageView;
@@ -18,7 +19,20 @@
 
 - (IBAction)closed:(id)sender;
 
-@property (weak, nonatomic) IBOutlet UILabel *customMemeLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *memeTextField;
+
+
+
+
+- (IBAction)saveButtonPressed:(id)sender;
+
+
+- (IBAction)viewPhotosPressed:(id)sender;
+
+
+- (IBAction)shareButtonPressed:(id)sender;
+
 
 
 @end
--- final_project/FreshPrince/FreshPrince/MemeCreatorViewController.m
+++ final_project/FreshPrince/FreshPrince/MemeCreatorViewController.m
@@ -7,23 +7,53 @@
 //
 
 #import "MemeCreatorViewController.h"
+#import <Social/Social.h>
 
 @interface MemeCreatorViewController ()
 
 @end
 
-@implementation MemeCreatorViewController
+@implementation MemeCreatorViewController {
+
+
+  
+    UIImage *imageForSharing;
+
+
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.memeTextField.layer.shadowOpacity = 1.0;
+    self.memeTextField.layer.shadowRadius = 0.0;
+    self.memeTextField.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.memeTextField.layer.shadowOffset = CGSizeMake(2.0, 1.0);
+    
     self.memeImageView.image = [UIImage imageNamed:self.memeImageName];
     
-    [self.customMemeLabel setFont:[UIFont fontWithName:@"Impact" size:10.0]];
+    [self.memeTextField setFont:[UIFont fontWithName:@"Impact" size:30.0]];
     
-    
+    self.memeImageView.userInteractionEnabled = YES;
+  
     
     // Do any additional setup after loading the view.
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+// It is important for you to hide the keyboard
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    
+    
+    
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,6 +77,105 @@
     
 }
 
+
+
+
+
+- (IBAction)saveButtonPressed:(id)sender {
+
+    
+    UIView *wholeScreen = self.memeImageView;
+    
+    [wholeScreen addSubview:self.memeTextField];
+    
+    // define the size and grab a UIImage from it
+    UIGraphicsBeginImageContextWithOptions(wholeScreen.bounds.size, wholeScreen.opaque, 0.0);
+    [wholeScreen.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIImageWriteToSavedPhotosAlbum(theImage, self,@selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
+   
+    
+}
+
+
+
+    
+
+
+- (IBAction)viewPhotosPressed:(id)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    
+    
+}
+
+- (IBAction)shareButtonPressed:(id)sender {
+   
+    UIView *wholeScreen = self.memeImageView;
+    [wholeScreen addSubview:self.memeTextField];
+    UIGraphicsBeginImageContextWithOptions(wholeScreen.bounds.size, wholeScreen.opaque, 0.0);
+    [wholeScreen.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *theImageforSharing = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+  
+    
+        if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+        
+            SLComposeViewController *facebookController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+            
+            [facebookController setInitialText:@"Check out my fresh meme"];
+            [facebookController addImage:theImageforSharing];
+            [self presentViewController:facebookController animated:YES completion:nil];
+        
+        
+        }
+    
+    
+    
+    
+}
+
+
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{
+    // Was there an error?
+    if (error != NULL)
+    {
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"My Alert"
+         message:@"This is an alert."
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+        [self presentViewController:alert animated:YES completion:nil];
+    } else {
+       
+        
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Success"
+                                                                       message:@"Image was successfully saved in photo album"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Close" style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * action) {}];
+        
+        [alert addAction:defaultAction];
+       [ self presentViewController:alert animated:YES completion:nil];
+        
+        
+        
+        
+    }
+}
 
 
 
